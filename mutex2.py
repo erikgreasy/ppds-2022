0dfb1c580512ad79b234195dbebc5a77cd75f2b8
erik.masny <erik.masny@gmail.com>
2022-02-21 08:55:33 +0100

    feat: implement second lock possibility

---
+++ mutex2.py
@@ -0,0 +1,39 @@
+from collections import Counter
+from dataclasses import asdict
+from fei.ppds import Thread, Mutex
+
+
+class Shared():
+    """Wrapper for array with related information about size and current index"""
+
+    def __init__(self, size):
+        self.counter = 0
+        self.end = size
+        self.elms = [0] * (size + 10)
+
+
+def do_count(shared):
+    """Execute writing to array with incrementing element and index"""
+    while True:
+        mutex.lock()
+        if shared.counter >= shared.end:
+            # unlock when loop ends to prevent not unlocked lock that causes
+            # deadlock
+            mutex.unlock()
+            break
+        shared.elms[shared.counter] += 1
+        shared.counter += 1
+        mutex.unlock()
+
+
+mutex = Mutex()
+shared = Shared(1_000_000)
+
+t1 = Thread(do_count, shared)
+t2 = Thread(do_count, shared)
+
+t1.join()
+t2.join()
+
+counter = Counter(shared.elms)
+print(counter.most_common())
