edf3a4a0e6e53dd94445f5ec117effd2d848d5fe
erik.masny <erik.masny@gmail.com>
2022-04-21 21:44:54 +0200

    feat: implement sync version of program

---
+++ sync.py
@@ -0,0 +1,23 @@
+import json
+import requests
+import time
+
+
+def get_jokerino(i):
+    res = requests.get('https://api.chucknorris.io/jokes/random').json()
+    print(f"{i}: {res['value']}")
+
+
+def main():
+
+    start = time.time()
+
+    for i in range(20):
+        get_jokerino(i)
+
+    end = time.time()
+    print(end - start)
+
+
+if __name__ == '__main__':
+    main()
